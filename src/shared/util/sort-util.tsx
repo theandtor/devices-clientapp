8ff64563f1972ded3261e18d841ae0520f70965f
Andres Ordonez <theandtor@gmail.com>
2021-12-14 03:01:34 -0500

    Fixed issue on sort number in hdd

--- src/shared/util/sort-util.tsx
+++ src/shared/util/sort-util.tsx
@@ -1,9 +1,28 @@
+import _, { lowerCase } from "lodash";
 import { IDeviceFilter } from "../model/device-filter.model";
 
 export const sortBy = (d1: any, d2: any, filter: IDeviceFilter): any => {
     const asc = filter.ascending ? 1 : -1;
     const desc = filter.ascending ? -1 : 1;
-    if (d1[filter.sort] < d2[filter.sort]) { return asc; }
-    if (d1[filter.sort] > d2[filter.sort]) { return desc; }
+
+    const { device1Value, device2Value } = getValueByPropName(d1, d2, filter.sort);
+
+    if (device1Value < device2Value) { return asc; }
+    if (device1Value > device2Value) { return desc; }
     return 0;
+}
+
+const getValueByPropName = (d1: any, d2: any, propName: string) => {
+    let device1Value = Number(d1[propName]) as number | string;
+    let device2Value = Number(d2[propName]) as number | string;
+
+    if (_.isNaN(device1Value) || _.isNaN(device2Value)) {
+        device1Value = lowerCase(d1[propName]);
+        device2Value = lowerCase(d2[propName]);
+    }
+
+    return {
+        device1Value,
+        device2Value,
+    }
 }
